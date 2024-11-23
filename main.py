7bf2c656d8995b38c985af055458f718f65673ac
Arabic Source <115221367+Tepthonee@users.noreply.github.com>
2024-11-23 09:02:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-"""هذا هو الكود الاساسي لا تقم بالتعديل علية"""
+"""هذا هو الكود الأساسي لا تقم بالتعديل عليه"""
 
 """py moa_yad.t.me GitHub@moa-yad"""
 
@@ -23,15 +23,15 @@ from telethon.sessions import StringSession
 
 api_id = os.environ.get("apiid")
 if api_id is not None and api_id != '':
-    print("تم تعيين الايبي ايدي .")
+    print("تم تعيين الأيبي أيدي .")
 else:
-    print("لم يتم تعيين الايبي ايدي .")
+    print("لم يتم تعيين الأيبي أيدي .")
 
 api_hash = os.environ.get("apihash")
 if api_hash is not None and api_hash != '':
-    print("تم تعيين الايبي هاش .")
+    print("تم تعيين الأيبي هاش .")
 else:
-    print("لم يتم تعيين الايبي هاش .")
+    print("لم يتم تعيين الأيبي هاش .")
 
 phone = os.environ.get("phone")
 if phone is not None and phone != '':
@@ -43,51 +43,51 @@ try:
     client = TelegramClient('session', api_id, api_hash)
     client.connect()
 except ValueError:
-    print('قم بملى المتغيرات بالكامل وبعدها اعد التشغيل .')
+    print('قم بملء المتغيرات بالكامل وبعدها أعد التشغيل .')
     exit()
 
 try:
     if not client.is_user_authorized():
         client.send_code_request(phone)
         try:
-            print("تم ارسال رمز التحقق الى حسابك ،")
-            code = input('قم بادخال رمز التحقق: ')
+            print("تم إرسال رمز التحقق إلى حسابك ،")
+            code = input('قم بإدخال رمز التحقق: ')
             client.sign_in(phone, code)
         except PhoneCodeInvalidError:
-            print('رمز التحقق خطأ ، حاول مرة اخرى .')
-            code = input('قم بادخال رمز التحقق: ')
+            print('رمز التحقق خطأ، حاول مرة أخرى .')
+            code = input('قم بإدخال رمز التحقق: ')
             client.sign_in(phone, code)
 except SessionPasswordNeededError:
-    print("!! تم تفعيل التحقق بخطوتين في حسابك مسبقاً !!")
-    password = input('قم بادخال رمز التحقق بخطوتين:  ')
+    print("!! تم تفعيل التحقق بخطوتين في حسابك مسبقًا !!")
+    password = input('قم بإدخال رمز التحقق بخطوتين:  ')
     try:
         client.sign_in(password=password)
     except PasswordHashInvalidError:
-        client.sign_in(password=input('الرمز خطأ ، حاول مرة اخرى:  '))
+        client.sign_in(password=input('الرمز خطأ، حاول مرة أخرى:  '))
 except PhoneCodeExpiredError:
-    print('تم انتهاء صلاحية رمز التحقق ، قم بطلب واحد جديد .')
+    print('تم انتهاء صلاحية رمز التحقق، قم بطلب واحد جديد .')
 except PhoneCodeInvalidError:
-    print('رمز تسجيل الدخول خطأ، قم بتشغيل الكود مرة اخرى .')
+    print('رمز تسجيل الدخول خطأ، قم بتشغيل الكود مرة أخرى .')
 except PasswordHashInvalidError:
-    print('رمز التحقق بخطوتين خطأ ، قم بتشغيل الكود مرة اخرى .')
+    print('رمز التحقق بخطوتين خطأ، قم بتشغيل الكود مرة أخرى .')
 except PhoneNumberBannedError:
-    print('رقم الهاتف محظور من التلجرام ، قم بتائكد من صحته.')
+    print('رقم الهاتف محظور من التلجرام، قم بالتأكد من صحته.')
 except PhoneNumberInvalidError:
-    print('رقم الهاتف خطأ ، قم بتصحيحة وحاول مره اخرى .')
+    print('رقم الهاتف خطأ، قم بتصحيحه وحاول مرة أخرى .')
 except PhoneNumberFloodError:
-    print('عدد كبير من محاولات تسجيل الدخول ، حاول بعد 24 ساعة .')
+    print('عدد كبير من محاولات تسجيل الدخول، حاول بعد 24 ساعة .')
 except ApiIdInvalidError:
-    print('ايبي ايدي/هاش  خطأ ، قم بتصحية وحاول مجدداً .')
+    print('أيبي أيدي/هاش  خطأ ، قم بتصحيحه وحاول مجددًا .')
 except FloodWaitError as e:
-    print( f'عدد كبير من محاولات تسجيل الدخول ، حاول بعد {e.seconds} ثانية .')
+    print( f'عدد كبير من محاولات تسجيل الدخول، حاول بعد {e.seconds} ثانية .')
 except Exception as e:
     print(f'خطأ غير معرف: {e}')
 try:
     session_string = StringSession.save(client.session)
     client.send_message(
         "me",
-        f"**هذا هو كود سيشن تيليثون الخاص بك**:\n\n`{session_string}`\n\n**لا تشارك هذا الكود مع اي مستخدم !**\n@jmthon",
+        f"**هذا هو كود سيشن تيليثون الخاص بك**:\n\n`{session_string}`\n\n**لا تشارك هذا الكود مع أي مستخدم !**\n@Tepthon",
         )
-    print("تم انشاء كود السيشن ، تم ارسالة الى الرسائل المحفوظة على التلجرام .")
+    print("تم إنشاء كود السيشن، تم إرساله إلى الرسائل المحفوظة على التلجرام .")
 except AuthKeyUnregisteredError:
-    print("فشل في انشاء كود السيشن ، حاول مرة اخرى.")
+    print("فشل في إنشاء كود السيشن، حاول مرة أخرى.")
